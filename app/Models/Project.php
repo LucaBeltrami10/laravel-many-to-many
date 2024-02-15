52c7779e1e479147acf4251ad4b84085f30ae311
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-15 17:24:11 +0100

    added methods in Project and Technology models

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -5,6 +5,7 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
+use App\Models\Technology;
 
 class Project extends Model
 {
@@ -24,5 +25,10 @@ class Project extends Model
     public function type()
     {
         return $this->belongsTo(Type::class);
+    }
+
+    public function technologies()
+    {
+        return $this->belongsToMany(Technology::class);
     }
 }
