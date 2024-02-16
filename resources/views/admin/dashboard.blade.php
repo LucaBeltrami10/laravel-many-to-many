ade0d314bfafb686924e0b10806e345fe63dddf9
LucaBeltrami10 <Lucabeltrami10@gmail.com>
2024-02-16 15:51:39 +0100

    added button in dashboard to show Technologies and Types

--- resources/views/admin/dashboard.blade.php
+++ resources/views/admin/dashboard.blade.php
@@ -12,6 +12,12 @@
     <a href="{{ route('admin.projects.deleted') }}">
         <button type="button" class="btn btn-dark">Eliminati di Recente</button>
     </a>
+    <a href="">
+        <button type="button" class="btn btn-dark">Technologies</button>
+    </a>
+    <a href="">
+        <button type="button" class="btn btn-dark">Types</button>
+    </a>
     
 </div>
 
